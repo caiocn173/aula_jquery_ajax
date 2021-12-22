415428c2b05155f5aabe77561ea2ba306661e4f8
Caio Silva <caiocnk173@gmail.com>
2021-12-22 13:21:53 -0300

    Classe navbar para desenvolvimento do cabeçalho do site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function consultaCEP(){
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/";
     console.log(url);
@@ -8,10 +9,22 @@ function consultaCEP(){
         success: function (response){
             console.log(response);
             $("#logradouro").html(response.logradouro);
-            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP: " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+            /* $("#logradouro").html(response.logradouro);
+            document.getElementById("logradouro").innerHTML = response.logradouro;
             document.getElementById("bairro").innerHTML = response.bairro;
             document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
+            document.getElementById("uf").innerHTML = response.uf; */
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
